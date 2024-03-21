79e58362ddd9f689cee025b10645b8e839920020
Arnab <pratihararnab270@gmail.com>
2024-03-21 12:47:17 +0530

    Final Commit

--- app/src/main/java/com/example/musicapp/MyAdapter.kt
+++ app/src/main/java/com/example/musicapp/MyAdapter.kt
@@ -3,6 +3,7 @@ package com.example.musicapp
 import android.app.Activity
 import android.content.Context
 import android.media.MediaPlayer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,53 +14,49 @@ import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
-class MyAdapter(val context:Activity, val dataList: List<Data>):
-RecyclerView.Adapter<MyAdapter.MyViewHolder>()   //extended RecyclerView.Adapter
-{
+class MyAdapter(val context: Activity, val dataList: List<Data>):
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        //create the view in case layout manager fails to create view for the data
-      val itemView=LayoutInflater.from(context).inflate(R.layout.each_item, parent,false)
-      return MyViewHolder(itemView)
-
+        // create the view in case thw layout manager fails to create view for the data
+        val itemView= LayoutInflater.from(context).inflate(R.layout.each_item, parent, false)
+        return MyViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {  //it returns the total no of items in the data set held by the adapter
-       return dataList.size
+    override fun getItemCount(): Int {
+        return dataList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        //populate the data(e.g Music Image,Music Ttile) into the view
-        val currentData=dataList[position]
-        val mediaPlayer=MediaPlayer.create(context,currentData.preview.toUri())
+        // populate the data into the view
+        val currentData= dataList[position]
 
-      //now have to fill details for each and every view
-        holder.title.text=currentData.title
+        val mediaPlayer= MediaPlayer.create(context, currentData.preview.toUri())
+        holder.title.text= currentData.title
 
         Picasso.get().load(currentData.album.cover).into(holder.image);
 
         holder.play.setOnClickListener {
-            mediaPlayer.start()  //music will play
+            mediaPlayer.start()
         }
+
         holder.pause.setOnClickListener {
-            mediaPlayer.stop()  //music will pause
+            mediaPlayer.stop()
         }
 
     }
 
-    class MyViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val image: ImageView
         val title: TextView
         val play: ImageButton
         val pause: ImageButton
 
         init {
-            image = itemView.findViewById(R.id.musicImage)
-            title = itemView.findViewById(R.id.musicTitle)
-            play = itemView.findViewById(R.id.btnPlay)
-            pause = itemView.findViewById(R.id.btnPause)
-
+            image= itemView.findViewById(R.id.musicImage)
+            title= itemView.findViewById(R.id.musicTitle)
+            play= itemView.findViewById(R.id.btnPlay)
+            pause= itemView.findViewById(R.id.btnPause)
         }
     }
-
 }
